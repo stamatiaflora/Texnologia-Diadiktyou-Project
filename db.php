42942e50c737a786f91ce58699cec5d201b2390b
stamatiaflora <stamzerva@gmail.com>
2026-06-06 18:53:04 +0300

    Update db.php

--- db.php
+++ db.php
@@ -1,11 +1,15 @@
 <?php
+
+// Στοιχεία σύνδεσης με βάση δεδομένων
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "warehouse_store";
 
+// Δημιουργία σύνδεσης 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
+// Έλεγχος επιτυχούς σύνδεσης 
 if ($conn->connect_error) {
     die("Η σύνδεση απέτυχε: " . $conn->connect_error);
 }
